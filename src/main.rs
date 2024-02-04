4f8d062701a71ac1b40f052a6840e7f43508a0c7
Yousef Moazzam <mrbenzedrine@live.co.uk>
2024-02-04 17:38:56 +0000

    Create graphics pipeline object

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ use vulkano::image::{Image, ImageCreateInfo, ImageType, ImageUsage};
 use vulkano::image::view::ImageView;
 use vulkano::format::Format;
 use vulkano::buffer::{Buffer, BufferContents, BufferCreateInfo, BufferUsage};
-use vulkano::pipeline::graphics::vertex_input::Vertex;
-use vulkano::render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass};
+use vulkano::pipeline::graphics::vertex_input::{Vertex, VertexDefinition};
+use vulkano::render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass, Subpass};
 use vulkano::command_buffer::allocator::{
     StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo,
 };
@@ -18,6 +18,15 @@ use vulkano::command_buffer::{
     AutoCommandBufferBuilder, CommandBufferUsage, PrimaryAutoCommandBuffer,
     RenderPassBeginInfo, SubpassBeginInfo, SubpassContents, SubpassEndInfo,
 };
+use vulkano::shader::ShaderModule;
+use vulkano::pipeline::{GraphicsPipeline, PipelineShaderStageCreateInfo, PipelineLayout};
+use vulkano::pipeline::layout::PipelineDescriptorSetLayoutCreateInfo;
+use vulkano::pipeline::graphics::GraphicsPipelineCreateInfo;
+use vulkano::pipeline::graphics::viewport::{Viewport, ViewportState};
+use vulkano::pipeline::graphics::input_assembly::InputAssemblyState;
+use vulkano::pipeline::graphics::rasterization::RasterizationState;
+use vulkano::pipeline::graphics::multisample::MultisampleState;
+use vulkano::pipeline::graphics::color_blend::{ColorBlendState, ColorBlendAttachmentState};
 
 #[derive(BufferContents, Vertex)]
 #[repr(C)]
@@ -87,6 +96,22 @@ fn main() {
         .expect("Should be able to create shader module for vertex shader");
     let fragment_shader = fragment_shaders::load(device.clone())
         .expect("Should be able to create shader module for fragment shader");
+
+    // Create viewport
+    let viewport = Viewport {
+        offset: [0.0, 0.0],
+        extent: [height as f32, width as f32],
+        depth_range: 0.0..=1.0,
+    };
+
+    // Create graphics pipeline object
+    let graphics_pipeline = create_graphics_pipeline(
+        device.clone(),
+        vertex_shader.clone(),
+        fragment_shader.clone(),
+        render_pass.clone(),
+        viewport.into(),
+    );
 }
 
 fn setup_instance() -> Arc<Instance> {
@@ -214,6 +239,68 @@ fn configure_command_buffer_builder(
         .expect("Should be able to configure image clearing in render pass")
         .end_render_pass(SubpassEndInfo::default())
         .expect("Should be able to configure end of render pass");
+}
+
+fn create_graphics_pipeline(
+    device: Arc<Device>,
+    vertex_shader: Arc<ShaderModule>,
+    fragment_shader: Arc<ShaderModule>,
+    render_pass: Arc<RenderPass>,
+    viewport: Viewport,
+) -> Arc<GraphicsPipeline> {
+    let vs = vertex_shader.entry_point("main")
+        .expect("Expected main() function in vertex shader");
+    let fs = fragment_shader.entry_point("main")
+        .expect("Expected main() function in fragment shader");
+
+    // TODO: Learn more, not much is said in vulkano triangle tutorial about this
+    let vertex_input_state = MyVertex::per_vertex()
+        .definition(&vs.info().input_interface)
+        .expect("Should be able to get vertex input state from vertex struct");
+
+    let stages = [
+        PipelineShaderStageCreateInfo::new(vs),
+        PipelineShaderStageCreateInfo::new(fs),
+    ];
+
+    // TODO: Learn more, not much is said in vulkano triangle tutorial about this
+    let pipeline_layout = PipelineLayout::new(
+        device.clone(),
+        PipelineDescriptorSetLayoutCreateInfo::from_stages(&stages)
+        .into_pipeline_layout_create_info(device.clone())
+        .expect("Should be able to create pipeline layout info")
+    ).expect("Should be able to create pipeline layout");
+
+    let subpass = Subpass::from(render_pass, 0)
+        .expect("Should be able to create single subpass");
+
+    let graphics_pipeline = GraphicsPipeline::new(
+        device.clone(),
+        None,
+        GraphicsPipelineCreateInfo {
+            // Need the `into_iter().collect()` to get a `smallvec::SmallVec`, not sure why can't
+            // create directly though
+            stages: stages.into_iter().collect(),
+            vertex_input_state: Some(vertex_input_state),
+            input_assembly_state: Some(InputAssemblyState::default()),
+            viewport_state: Some(ViewportState {
+                // Need the `into_iter().collect()` to get a `smallvec::SmallVec`, not sure why
+                // can't create directly though
+                viewports: [viewport].into_iter().collect(),
+                ..Default::default()
+            }),
+            rasterization_state: Some(RasterizationState::default()),
+            multisample_state: Some(MultisampleState::default()),
+            color_blend_state: Some(ColorBlendState::with_attachment_states(
+                subpass.num_color_attachments(),
+                ColorBlendAttachmentState::default(),
+            )),
+            subpass: Some(subpass.into()),
+            ..GraphicsPipelineCreateInfo::layout(pipeline_layout)
+        },
+    ).expect("Should be able to create graphics pipeline object");
+
+    graphics_pipeline
 }
 
 mod vertex_shaders {
